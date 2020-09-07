16ddf6ecb04b7381f40ef2e1bbd0848584cd9719
atmajayadw <atmajaya.dw@gmail.com>
2020-09-07 18:04:45 +0700

    initialize git

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.min.css">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
+
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="lib/css/style.css">
+    <link rel="stylesheet" href="lib/animatecss/animate.css">
+
+    <title>SMA YASPEN TUGU IBU</title>
+</head>
+
+<body>
+
+    <h1>Hello World!</h1>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src=" lib/bootstrap/js/jquery.js"> </script>
+    <script src="lib/bootstrap/js/popper.js">
+    </script>
+    <script src="lib/bootstrap/js/bootstrap.js"></script>
+    <!--  -->
+
+    <!-- Optional JavaScript -->
+    <script src="lib/wowjs/wow.min.js"></script>
+    <script src="lib/jqueryeasing/jquery.easing.1.3.js"></script>
+    <script src="lib/js/script.js"></script>
+
+</body>
+
+</html>
