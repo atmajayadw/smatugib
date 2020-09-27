b3b1b8835a5392375a38924028fb08e9c42276b7
atmajayadw <atmajaya.dw@gmail.com>
2020-09-27 17:59:57 +0700

    initialize admin panel

---
+++ admin/content.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CONTENT MANAGEMENT - SMA TUGU IBU</title>
+</head>
+
+<body>
+    <h2>Content Management</h2>
+</body>
+
+</html>
